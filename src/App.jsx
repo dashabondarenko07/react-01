50c848235ebc768006f2b7bbcb6728a8fc1df3cc
dashabondarenko07 <dashabondarenko0709@gmail.com>
2022-06-22 01:24:34 +0300

    Day01

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import paintings from '../paintings.json';
+import Paintings from './components/Paintings.js';
+
+export default function App() {
+    return (
+        <div>
+              <Paintings
+    key={paintings.id}
+    imageUrl={paintings.url}
+    title={paintings.title}
+    price={paintings.price}
+    authorTag={paintings.author.tag}
+    authorUrl={paintings.author.url}
+    quantity={paintings.quantity}
+  />
+        </div>
+    )
+}
---
+++ src/components/Paintings.js
@@ -0,0 +1,16 @@
+export default function Paintings(props) {
+  const { key, imageUrl, title, price, authorTag, authorUrl, quantity } = props;
+
+  return (
+    <div>
+      <img src={imageUrl} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Author: <a href={authorUrl}>{authorTag}</a>
+      </p>
+      <p>Price: {price} credits</p>
+      {/* <p>Availability: {quantity} running out or in stock</p> */}
+      <button type="button">Add to Basket</button>
+    </div>
+  );
+}
